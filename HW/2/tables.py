a12890ef9da95d4693bf976bd7db939aef13ae9f
cregan7039 <cregan7039@sohamcollege.org.uk>
2014-06-19 18:09:27 +0100

    Update tables.py

--- HW/2/tables.py
+++ HW/2/tables.py
@@ -1,1 +1,7 @@
-#paste code here
+userNum = int(input("Enter a number of which you would like to find the timestable for: ")) #User enters number for the timestable the wish to see.
+number = 1 #Number that gets multiplied by the users number
+while number != 13: #Loop stops when number equals 13
+    overall = userNum * number #This times the users number by num
+    print(overall) #Prints out the overall number
+    number = number + 1 #This steps number up so when it loops through everything num goes up one which makes the timestable increase
+
