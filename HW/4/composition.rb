1e7ea9f2d8135f29d87adc3611df360777ebaddb
cregan7039 <cregan7039@sohamcollege.org.uk>
2014-07-03 20:51:28 +0100

    Sonic Pi Homework

--- HW/4/composition.rb
+++ HW/4/composition.rb
@@ -1,1 +1,23 @@
 #paste code here
+in_thread do
+  with_synth "beep"
+  2.times do
+    play 60
+    sleep 0.5
+    play 67
+    sleep 0.5
+    play 60
+    sleep 0.5
+    play 63
+    sleep 0.5
+  end
+end
+    
+    
+in_thread do
+  with_synth "pretty_bell"
+  30.times do
+    play 49
+    sleep 1
+  end
+end
